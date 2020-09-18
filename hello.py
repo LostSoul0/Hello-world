212893992bcc249b8caf0f60955208d2a8afe231
Jakub <jakub.suchenek.25@gmail.com>
2020-09-18 15:47:44 +0200

    Add comments and check "__main__" variable
    Also set default encoding and added last line enter. (It's a good practice for all files.)
    I'm working with Python for years, and in my opinion it's the most documented script I've ever found. 😅

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-print("Hello world!")
+# -*- coding: utf-8 -*-
+# Be sure to specify file encoding
+
+if __name__ == '__main__':  # Check if script is executed directly (not by import)
+  print("Hello world!")  # And simply use Python build-in function
